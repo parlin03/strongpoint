798810f24fc820448ee45e0590947334e62170da
unknown <parlin03@gmail.com>
2025-07-20 04:01:55 +0800

    opd

---
+++ application/controllers/opd.php
@@ -0,0 +1,102 @@
+<?php
+
+use LDAP\Result;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class opd extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		// is_logged_in();
+		$this->load->library('session');
+		$this->load->model('M_opd', 'opd_model');
+	}
+
+
+	public function instansi($opd)
+	{
+		$data['opd'] = urldecode($opd);
+		$data['menu'] = '';
+		$data['title'] = ucwords($data['opd']);
+		$data['user'] = $this->db->get_where('users', ['email' => $this->session->userdata('email')])->row_array(); //arraynya sebaris
+
+		// $this->session->set_userdata('opd', urldecode($opd));
+		$this->session->set_flashdata('opd', $data['opd']);
+		$data['summary'] = $this->opd_model->getDataSummary($data['opd']);
+		$data['bidang'] = $this->db->query("SELECT bidang FROM opd WHERE opd = '" . $data['opd'] . "' GROUP by bidang;")->num_rows();
+		$data['perusahaan'] = $this->db->query("SELECT perusahaan FROM opd WHERE opd = '" . $data['opd'] . "' GROUP by perusahaan;")->num_rows();
+		$data['merk'] = $this->db->query("SELECT Merk FROM opd WHERE opd = '" . $data['opd'] . "'  GROUP by Merk")->num_rows();
+		$data['shu'] = $this->db->query("SELECT shu FROM opd WHERE opd = '" . $data['opd'] . "' GROUP by `shu` ")->num_rows();
+		$data['nilai_shu'] = $this->db->query("SELECT nilai_shu FROM `opd` WHERE opd = '" . $data['opd'] . "' GROUP by `nilai_shu` ")->num_rows();
+		$data['ecatalog'] = $this->db->query("SELECT ecatalog FROM `opd` WHERE opd = '" . $data['opd'] . "' GROUP by `ecatalog` ")->num_rows();
+		// var_dump($data['merk']);
+		// var_dump($this->db->last_query());
+		// die();
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('v_opd', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function Graph_list()
+	{
+		header('Content-Type: application/json');
+		$opd = urldecode($this->session->flashdata('opd'));
+		// var_dump($opd);
+		// die();
+		$graph = $this->opd_model->getDataGraph($opd);
+
+		$rows = array();
+		foreach ($graph as $d) {
+			array_push($rows, array($d->pekerjaan, $d->total));
+		}
+
+		print json_encode($rows, JSON_NUMERIC_CHECK);
+	}
+
+
+	public function ajax_list()
+	{
+		header('Content-Type: application/json');
+		// $opd = urldecode($this->uri->segment(3));
+		// $opd = urldecode($this->session->flashdata('opd'));
+		$list = $this->opd_model->get_datatables();
+		$data = array();
+		$no = $this->input->post('start');
+		//looping data mahasiswa
+		foreach ($list as $list) {
+			$no++;
+			$row = array();
+			//row pertama akan kita gunakan untuk btn edit dan delete
+			$row[] =  $no;
+			$row[] = $list->pekerjaan;
+			$row[] = $list->jenis_pekerjaan;
+			$row[] = $list->bidang;
+			$row[] = $list->perusahaan;
+			$row[] = $list->Merk;
+			$row[] = $list->pagu_anggaran;
+			$row[] = $list->ppn;
+			$row[] = $list->ppn_pph;
+			$row[] = $list->rela_cost;
+			$row[] = $list->shu;
+			$row[] = $list->nilai_shu;
+			$row[] = '<a href="' . $list->ecatalog . '">' . $list->ecatalog . '</a>';
+			$row[] = $list->pic;
+			$row[] =  '<a class="btn btn-success btn-sm"><i class="fa fa-edit"></i> </a>
+            <a class="btn btn-danger btn-sm "><i class="fa fa-trash"></i> </a>';
+			$data[] = $row;
+		}
+
+		$output = array(
+			"draw" => $this->input->post('draw'),
+			"recordsTotal" => $this->opd_model->count_all(),
+			"recordsFiltered" => $this->opd_model->count_filtered(),
+			"data" => $data,
+		);
+		//output to json format
+		$this->output->set_output(json_encode($output));
+	}
+}
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,17 +1,17 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-light-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="<?= base_url('home'); ?>" class="brand-link ">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <!-- <img src="<?= base_url('assets/img/favicon.ico') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-        </div>
-        <span class="brand-text   mx-2">Strong Point</span>
-    </a>
+	<!-- Brand Logo -->
+	<a href="<?= base_url('home'); ?>" class="brand-link ">
+		<div class="sidebar-brand-icon rotate-n-15">
+			<!-- <img src="<?= base_url('assets/img/favicon.ico') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+		</div>
+		<span class="brand-text   mx-2">Strong Point</span>
+	</a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+	<!-- Sidebar -->
+	<div class="sidebar">
+		<!-- Sidebar user panel (optional) -->
+		<!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="<?= base_url('assets/') ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
@@ -20,120 +20,145 @@
             </div>
         </div> -->
 
-        <!-- Sidebar Menu -->
+		<!-- Sidebar Menu -->
 
-        <nav class="mt-2">
+		<nav class="mt-2">
 
-            <ul class="nav nav-pills nav-sidebar nav-child-indent flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
+			<ul class="nav nav-pills nav-sidebar nav-child-indent flex-column" data-widget="treeview" role="menu" data-accordion="false">
+				<!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
 
-                <!-- Query Menu -->
-                <?php
-                $role_id = $this->session->userdata('role_id');
-                $queryMenu = "SELECT 
+				<!-- Query Menu -->
+				<?php
+				$role_id = $this->session->userdata('role_id');
+				$queryMenu = "SELECT 
                             `user_menu`.`id`, `menu` FROM `user_menu` JOIN `user_access_menu` ON
                             `user_menu`.`id` = `user_access_menu`.`menu_id` WHERE
                             `user_access_menu`.`role_id`= $role_id ORDER BY `user_access_menu`.`menu_id`
                             ASC ";
-                $menu = $this->db->query($queryMenu)->result_array(); ?>
-                <!-- Looping Menu -->
-                <?php foreach ($menu as $m) : ?>
-                    <div class="user-panel mt-2 pb-2 mb-2 ">
-                        <!-- <li class="nav-item"><?= $m['menu']; ?></li> -->
+				$menu = $this->db->query($queryMenu)->result_array(); ?>
+				<!-- Looping Menu -->
+				<?php foreach ($menu as $m) : ?>
+					<div class="user-panel mt-2 pb-2 mb-2 ">
+						<!-- <li class="nav-item"><?= $m['menu']; ?></li> -->
 
 
-                        <?php
-                        $menuId = $m['id'];
-                        $querySubMenu = "SELECT  `user_sub_menu`.`id`, `user_sub_menu`.`menu_id`,`user_sub_menu`.`title`,
+						<?php
+						$menuId = $m['id'];
+						$querySubMenu = "SELECT  `user_sub_menu`.`id`, `user_sub_menu`.`menu_id`,`user_sub_menu`.`title`,
                         `user_sub_menu`.`url`, `user_sub_menu`.`icon`, `user_sub_menu`.`is_active`, `menu` 
                         FROM `user_sub_menu` JOIN `user_menu`  
                         ON `user_sub_menu`.`menu_id`  = `user_menu`.`id`
                         WHERE `user_sub_menu`.`menu_id`= $menuId
                         AND `user_sub_menu`.`is_active` = 1 order by `user_sub_menu`.`id`";
-                        $subMenu = $this->db->query($querySubMenu)->result_array(); //
-                        // var_dump($subMenu);
-                        ?>
-                        <?php foreach ($subMenu as $sm) : ?>
+						$subMenu = $this->db->query($querySubMenu)->result_array(); //
+						// var_dump($subMenu);
+						?>
+						<?php foreach ($subMenu as $sm) : ?>
 
-                            <?php
-                            $subMenuId = $sm['id'];
-                            // echo $subMenuId;
-                            $queryAltSubMenu = "SELECT * 
+							<?php
+							$subMenuId = $sm['id'];
+							// echo $subMenuId;
+							$queryAltSubMenu = "SELECT * 
                                 FROM `user_alt_submenu` JOIN `user_sub_menu`  
                                 ON `user_alt_submenu`.`submenu_id`  = `user_sub_menu`.`id`
                                 WHERE `user_alt_submenu`.`submenu_id`= $subMenuId
                                 AND `user_alt_submenu`.`is_active` = 1
                                 ORDER BY `user_alt_submenu`.`id` ASC ";
-                            $altSubMenu = $this->db->query($queryAltSubMenu);
-                            //                var_dump($altSubMenu); 
-                            ?>
+							$altSubMenu = $this->db->query($queryAltSubMenu);
+							//                var_dump($altSubMenu); 
+							?>
 
-                            <?php if ($altSubMenu->num_rows() < 1) : ?>
-                                <li class="nav-item">
-                                    <a href="<?= base_url($sm['url']); ?>" class="nav-link  <?= ($title == $sm['title']) ? 'active' : ''; ?>">
-                                        <i class="<?= $sm['icon']; ?>"></i>
-                                        <p><?= $sm['title']; ?></p>
-                                    </a>
-                                </li>
-                            <?php else : ?>
-                                <?php
-                                $uri = $this->uri->segment(1) . "/" . $this->uri->segment(2);
-                                if ($uri == $sm['url']) {
-                                    $show = 'menu-open';
-                                    $active = 'active';
-                                } else {
-                                    $show = '';
-                                    $active = '';
-                                }
-                                ?>
-                                <li class="nav-item has-treeview <?= $show ?>">
-                                    <!-- <?php var_dump($title);
-                                            var_dump($sm['title']);
-                                            ?> -->
+							<?php if ($altSubMenu->num_rows() < 1) : ?>
+								<li class="nav-item">
+									<a href="<?= base_url($sm['url']); ?>" class="nav-link  <?= ($title == $sm['title']) ? 'active' : ''; ?>">
+										<i class="<?= $sm['icon']; ?>"></i>
+										<p><?= $sm['title']; ?></p>
+									</a>
+								</li>
+							<?php else : ?>
+								<?php
+								$uri = $this->uri->segment(1) . "/" . $this->uri->segment(2);
+								if ($uri == $sm['url']) {
+									$show = 'menu-open';
+									$active = 'active';
+								} else {
+									$show = '';
+									$active = '';
+								}
+								?>
+								<li class="nav-item has-treeview <?= $show ?>">
+									<!-- <?php var_dump($title);
+											var_dump($sm['title']);
+											?> -->
 
-                                    <a href="<?= base_url($sm['url']); ?>" class="nav-link <?= $active ?>">
-                                        <i class="<?= $sm['icon']; ?>"></i>
-                                        <p>
-                                            <?= $sm['title']; ?>
-                                            <i class="right fas fa-angle-left"></i>
-                                        </p>
-                                    </a>
-                                    <?php $rasm = $altSubMenu->result_array(); ?>
-                                    <!-- <div class="bg-white py-2 collapse-inner rounded"> -->
-                                    <?php foreach ($rasm as $asm) : ?>
-                                        <ul class="nav nav-treeview  nav-child-indent">
-                                            <li class="nav-item ">
-                                                <a href="<?= base_url($asm['alt_url']); ?>" class="nav-link <?= ($asm['alt_url'] == uri_string()) ? 'active bg-primary' : ''; ?>">
-                                                    <!-- <i class="far fa-circle nav-icon"></i> -->
-                                                    <p><?= $asm['alt_title']; ?></p>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    <?php endforeach; ?>
-                                    <!-- </div> -->
-                                </li>
+									<a href="<?= base_url($sm['url']); ?>" class="nav-link <?= $active ?>">
+										<i class="<?= $sm['icon']; ?>"></i>
+										<p>
+											<?= $sm['title']; ?>
+											<i class="right fas fa-angle-left"></i>
+										</p>
+									</a>
+									<?php $rasm = $altSubMenu->result_array(); ?>
+									<!-- <div class="bg-white py-2 collapse-inner rounded"> -->
+									<?php foreach ($rasm as $asm) : ?>
+										<ul class="nav nav-treeview  nav-child-indent">
+											<li class="nav-item ">
+												<a href="<?= base_url($asm['alt_url']); ?>" class="nav-link <?= ($asm['alt_url'] == uri_string()) ? 'active bg-primary' : ''; ?>">
+													<!-- <i class="far fa-circle nav-icon"></i> -->
+													<p><?= $asm['alt_title']; ?></p>
+												</a>
+											</li>
+										</ul>
+									<?php endforeach; ?>
+									<!-- </div> -->
+								</li>
 
-                            <?php endif; ?>
+							<?php endif; ?>
 
-                        <?php endforeach; ?>
-                    </div>
-                <?php endforeach; ?>
+						<?php endforeach; ?>
 
 
+					</div>
+				<?php endforeach; ?>
+				<div class="user-panel mt-2 pb-2 mb-2 ">
+					<li class="nav-item has-treeview menu-open">
+						<a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+							<i class="fas fa-building fa-fw"></i>
+							<span>OPD</span></a>
 
-                <!-- <hr class="sidebar-divider d-none d-md-block" /> -->
-                <!-- Nav Item - Logout -->
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-fw fa-sign-out-alt"></i>
-                        <span>Logout</span></a>
-                </li>
+						<?php $list_opd = $this->db->query("SELECT opd FROM `opd` GROUP by opd")->result_array(); ?>
+						<?php foreach ($list_opd as $o) : ?>
+							<ul class="nav nav-treeview  nav-child-indent">
+								<li class="nav-item ">
+									<a href="<?= base_url('opd/instansi/' . strtolower($o['opd'])); ?>" class="nav-link <?= (strtolower($o['opd']) == (strtolower(urldecode($this->uri->segment(3))))) ? 'active bg-primary' : ''; ?>">
+										<?php
+										// var_dump(($list_opd == (ucwords(urldecode($this->uri->segment(3))))));
+										// var_dump($list_opd);
+										// var_dump((ucwords(urldecode($this->uri->segment(3)))));
+										// die();
+										?>
+										<i class="far fa-circle nav-icon"></i>
+										<p><?= $o['opd']; ?></p>
+									</a>
+								</li>
+							</ul>
+						<?php endforeach; ?>
+					</li>
+				</div>
+				<!-- <hr class="sidebar-divider d-none d-md-block" /> -->
+				<!-- Nav Item - Logout -->
+				<li class="nav-item">
+					<a class="nav-link" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
+						<i class="fas fa-fw fa-sign-out-alt"></i>
+
+						<span>Logout</span></a>
+				</li>
 
 
-            </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+			</ul>
+		</nav>
+		<!-- /.sidebar-menu -->
+	</div>
+	<!-- /.sidebar -->
 </aside>
